9521b4020fb4128ffcc9e0abaed9a118e948ab0b
tererico <teresa.rico@iest.edu.mx>
2020-11-28 19:08:54 -0600

    Add post method in contact form

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const NoticiaController = require("../controllers/NoticiaController");
+const nodemailer = require('nodemailer');
+const GMAIL_USER = process.env.GMAIL_USER;
+const GMAIL_PASS = process.env.GMAIL_PASS;
+
 
 router.get("/", NoticiaController.indexLatestNoticias);
 
@@ -11,6 +15,39 @@ router.get("/faqs", (req, res) => {
 router.get("/contacto", (req, res) => {
   res.render("contacto");
 });
+
+// POST del form contacto
+router.post('/contacto', (req, res) => {
+
+  // MTP server
+  const smtpTrans = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+      user: GMAIL_USER,
+      pass: GMAIL_PASS
+    }
+  })
+
+  // Correo que se mandará
+  const mail = {
+    from: 'Your sender info here', // This is ignored by Gmail
+    to: GMAIL_USER,
+    subject: 'Nuevo mensaje de contacto de ISND',
+    text: `${req.body.nombre} (${req.body.correo}) says: ${req.body.mensaje}`
+  }
+
+  // Enviar correo
+  smtpTrans.sendMail(mail, (error, response) => {
+    if (error) {
+      res.render('contacto') 
+    }
+    else {
+      res.redirect('/') // Regresa a index
+    }
+  })
+})
 
 router.get("/nosotros", NoticiaController.nosotrosLatestNoticias);
 
