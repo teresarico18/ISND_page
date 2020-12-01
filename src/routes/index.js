b38365c84fa56a74605965fe8ac2a1235a4609ea
josh231101 <wiijosue333@gmail.com>
2020-12-01 15:18:15 -0600

    Add message when contact error

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const NoticiaController = require("../controllers/NoticiaController");
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 const GMAIL_USER = process.env.GMAIL_USER;
 const GMAIL_PASS = process.env.GMAIL_PASS;
-
 
 router.get("/", NoticiaController.indexLatestNoticias);
 
@@ -13,47 +12,40 @@ router.get("/faqs", (req, res) => {
 });
 
 router.get("/contacto", (req, res) => {
-  res.render("contacto");
+  res.render("contacto", { error: false });
 });
 
 // POST del form contacto
-router.post('/contacto', (req, res) => {
-
+router.post("/contacto", (req, res) => {
   // MTP server
   const smtpTrans = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
+    host: "smtp.gmail.com",
     port: 465,
     secure: true,
     auth: {
       user: GMAIL_USER,
-      pass: GMAIL_PASS
-    }
-  })
+      pass: GMAIL_PASS,
+    },
+  });
 
   // Correo que se mandará
   const mail = {
-    from: 'Your sender info here', // This is ignored by Gmail
+    from: "Your sender info here", // This is ignored by Gmail
     to: GMAIL_USER,
-    subject: 'Nuevo mensaje de contacto de ISND',
-    text: `${req.body.nombre} (${req.body.correo}) says: ${req.body.mensaje}`
-  }
+    subject: "Nuevo mensaje de contacto de ISND",
+    text: `${req.body.nombre} (${req.body.correo}) says: ${req.body.mensaje}`,
+  };
 
   // Enviar correo
   smtpTrans.sendMail(mail, (error, response) => {
-    if (error) {
-      res.render('contacto') 
-    }
-    else {
-      res.redirect('/') // Regresa a index
-    }
-  })
-})
+    error ? res.render("contacto", { error: true }) : res.redirect("/");
+  });
+});
 
 router.get("/nosotros", NoticiaController.nosotrosLatestNoticias);
 
 router.get("/proyectos", (req, res) => {
   res.render("proyectos");
 });
-
 
 module.exports = router;
