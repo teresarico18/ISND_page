b88c6147001216a1b12c14e3746746a9143776d0
Josué Arreola <wiijosue333@gmail.com>
2020-12-07 22:22:04 -0600

    Delete extra files and logs

--- src/utils/convertArrayToObjectByKey.js
+++ src/utils/convertArrayToObjectByKey.js
@@ -1,6 +1,5 @@
 module.exports = (data, key) => {
   let result = {};
-  console.log(data);
   data.forEach((element) => {
     if (result.hasOwnProperty(element[key])) {
       result[element[key]].push(element);
